3f102d9aa29e10ebd38e51b54805f2b7a20bb09e
Alex Sutton <alexsutton14@hotmail.com>
2020-03-17 12:30:08 +0000

    Complete up to HTML Form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true
-    validates :email, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
     validates :password, presence: true
 end
